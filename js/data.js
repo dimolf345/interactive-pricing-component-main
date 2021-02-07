2aa2270bd8b6078c45f64f71eec4825b4853cbf3
dimolf345 <dimolfetta.ta@gmail.com>
2021-02-07 20:27:33 +0100

    project completed

---
+++ js/data.js
@@ -0,0 +1,24 @@
+const prices = [
+   {
+      pageviews: '10K',
+      price: 8
+   },
+   {
+      pageviews: '50K',
+      price: 12
+   },
+   {
+      pageviews: '100K',
+      price: 16
+   },
+   {
+      pageviews: '500K',
+      price: 24
+   },
+   {
+      pageviews: '1M',
+      price: 36
+   }
+];
+
+export { prices };
--- js/main.js
+++ js/main.js
@@ -1,23 +1,57 @@
+import { prices } from './data.js';
+
+
+function showBilling(arrayIndex) {
+  let price = (toggle.checked)? (prices[arrayIndex].price * 0.75) : prices[arrayIndex].price;
+  pageViews.textContent = prices[arrayIndex].pageviews + ' pageviews';
+
+  billing.textContent = "$ " + price.toFixed(2);
+}
+
 function colorChromeSlider(rangeValue) {
   sliderStyle.innerHTML = `#slider::-webkit-slider-runnable-track {
     background: linear-gradient(to right, #A1F2EA ${rangeValue}%, #F1F5FE ${rangeValue}% 100%);}`;
 }
 
-
-function calcSliderProgress() {
+function calcSliderProgress(sliderValue) {
   //returns the progress of the rangeSlider input in % based on range.value
-  return (rangeSlider.value * 100 / (rangeSlider['max'] - rangeSlider['min']));
+  return (sliderValue * 100 / (rangeSlider['max'] - rangeSlider['min']));
 }
 
+let toggle = document.getElementById('subscriptionToggle');
+let pageViews = document.querySelector('.first');
+let billing = document.getElementById('billing');
 let rangeSlider = document.getElementById('slider');
 let sliderContainer = document.getElementById('sliderContainer');
 let percentage = calcSliderProgress();
-/*Following statement will be used to update webkit-slider-runnable-track for CHROME and EDGE browsers*/
+let button = document.getElementsByTagName('button')[0];
+
+/*Following statements will be used to update webkit-slider-runnable-track for CHROME and EDGE browsers*/
 let sliderStyle = document.createElement('style');
 colorChromeSlider(percentage);
 sliderContainer.append(sliderStyle);
+/**/
+showBilling(rangeSlider.value);
+
+rangeSlider.addEventListener('input', function (e) {
+
+  colorChromeSlider(calcSliderProgress(e.target.value));
+  showBilling(e.target.value);
+})
+
+toggle.addEventListener('change', function (e) {
+  showBilling(rangeSlider.value);
+})
 
 
-rangeSlider.addEventListener('input', function(e) {
-  colorChromeSlider(calcSliderProgress());
+
+/*To display a message when user  clicks a button*/
+button.addEventListener('click', function(e) {
+  e.preventDefault();
+  let form = button.parentNode;
+  let message = document.createElement('h1');
+  message.textContent = "Thank you for choosing us!";
+  form.innerHTML = '';
+  form.appendChild(message);
+
 })
